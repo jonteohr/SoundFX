fe4d4975d9d8e1c3d398aa8f73596186a16af885
Jonathan Öhrström <tacco_9@hotmail.com>
2016-12-29 18:42:58 +0100

    Added plugin & source

---
+++ source/src/net/condolent/SFXEvents.java
@@ -0,0 +1,115 @@
+package net.condolent;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+public class SFXEvents implements Listener {
+
+	SFXMain plugin;
+	
+	public SFXEvents(SFXMain instance) {
+		plugin = instance;
+	}
+	
+	public FileConfiguration getConfig() {
+		return plugin.getConfig();
+	}
+	
+	public Sound chat = Sound.ENTITY_EXPERIENCE_ORB_PICKUP;
+	public Sound join = Sound.BLOCK_ANVIL_USE;
+	public Sound quit = Sound.BLOCK_ANVIL_HIT;
+	public Sound respawn = Sound.BLOCK_STONE_BUTTON_CLICK_ON;
+	public Sound tp = Sound.BLOCK_PORTAL_TRAVEL;
+	
+	@EventHandler
+	public void onMsgSound(AsyncPlayerChatEvent e) {
+		/*Player p = (Player) Bukkit.getOnlinePlayers();
+		Location location = p.getLocation();*/
+
+		if(getConfig().getBoolean("chatSound")) {
+			for(Player allPlayers : Bukkit.getOnlinePlayers()) {
+				allPlayers.playSound(allPlayers.getLocation(), chat, 1, 0);
+			}
+		} else {
+			// Do nothing
+		}
+	}
+	
+	// Play sound on player join
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e) {
+		// Play a sound on join?
+		if(getConfig().getBoolean("playerJoinSound")) {
+			for(Player allPlayers : Bukkit.getOnlinePlayers()) {
+				allPlayers.playSound(allPlayers.getLocation(), join, 1, 0);
+			}
+		} else {
+			// Do nothing
+		}
+	}
+	
+	// Play sound on player leave
+	@EventHandler
+	public void onQuit(PlayerQuitEvent e) {
+		
+		if(getConfig().getBoolean("playerQuitSound")) {
+			
+			for(Player allPlayers : Bukkit.getOnlinePlayers()) {
+				
+				allPlayers.playSound(allPlayers.getLocation(), quit, 1, 0);
+				
+			}
+			
+		} else {
+			// Do nothing
+		}
+		
+	}
+	
+	// Play sound on teleport
+	@EventHandler
+	public void onTp(PlayerTeleportEvent e) {
+		Player p = e.getPlayer();
+		
+		if(getConfig().getBoolean("playerTP")) {
+			p.playSound(p.getLocation(), tp, 1, 0);
+		} else {
+			// Do nothing
+		}
+		
+	}
+	
+	// Play sound on respawn
+	@EventHandler
+	public void onRespawn(PlayerRespawnEvent e) {
+		
+		if(getConfig().getBoolean("playerRespawn")) {
+			
+			// Play respawn sound to everyone
+			if(getConfig().getString("respawnSound").equalsIgnoreCase("all")) {
+				
+				for(Player p : Bukkit.getOnlinePlayers()) {
+					p.playSound(p.getLocation(), respawn, 1, 0);
+				}
+				
+			} else if(getConfig().getString("respawnSound").equalsIgnoreCase("player")) {
+				Player p = e.getPlayer();
+				
+				p.playSound(p.getLocation(), respawn, 1, 0);
+			}
+			
+		} else {
+			// Do nothing
+		}
+		
+	}
+}
---
+++ source/src/net/condolent/SFXMain.java
@@ -0,0 +1,130 @@
+package net.condolent;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SFXMain extends JavaPlugin {
+
+	public Permission admin = new Permission("sfx.admin");
+
+	private static Plugin plugin;
+	public static Plugin getPlugin() {
+		return plugin;
+	}
+	
+	SFXEvents events;
+	
+	public void onEnable() {
+		getLogger().info("SFX enabled!");
+
+		plugin = this;
+		
+		this.getServer().getPluginManager().registerEvents(new SFXEvents(this), this);
+
+		this.getConfig().options().copyDefaults(false);
+		saveDefaultConfig();
+
+	}
+
+	public void onDisable() {
+		getLogger().info("Disabling SFX..");
+		saveDefaultConfig();
+	}
+	
+	private String prefix = ChatColor.BOLD + "" + ChatColor.GOLD + "[SFX] ";
+	private String noAccess = prefix + ChatColor.RED + "Insufficient permissions.";
+	private String sep = "-------------------------------";
+	private String incorrectArg = prefix + ChatColor.RED + "Incorrect arguments.";
+
+	PluginDescriptionFile pdf = this.getDescription();
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player p = (Player) sender;
+		Location location = p.getLocation();
+
+		if (cmd.getName().equalsIgnoreCase("sfx")) {
+			
+			if (args.length < 1) {
+				p.sendMessage(sep);
+				p.sendMessage(prefix + ChatColor.RED + "Not enough arguments..");
+				p.sendMessage(prefix + ChatColor.YELLOW + "Correct usage: /sfx <argument>");
+				p.sendMessage(prefix + ChatColor.YELLOW + "Available arguments: version, reload, info, preview");
+				p.sendMessage(sep);
+			} else if (args.length == 1) {
+				
+				if (args[0].equalsIgnoreCase("version")) {
+					p.sendMessage(prefix + ChatColor.YELLOW + "This server is running version " + pdf.getVersion() + "of " + pdf.getName());
+				}
+				
+				if (args[0].equalsIgnoreCase("reload")) {
+					if (p.hasPermission(admin)) {
+						reloadConfig();
+						p.sendMessage(prefix + ChatColor.GREEN + "Config reloaded!");
+					} else {
+						p.sendMessage(noAccess);
+					}
+				}
+				
+				if(args[0].equalsIgnoreCase("info")) {
+					p.sendMessage(sep);
+					p.sendMessage(prefix + ChatColor.YELLOW + "This plugin was made by Condolent.");
+					p.sendMessage(prefix + ChatColor.YELLOW + "Originally made for the EverFree server per request,");
+					p.sendMessage(prefix + ChatColor.YELLOW + "then released to the public with permission!");
+					p.sendMessage(prefix + ChatColor.AQUA + "http://twitter.com/hyprcsgo");
+					p.sendMessage(sep);
+				}
+				
+				if(args[0].equalsIgnoreCase("preview")) {
+					p.sendMessage(prefix + ChatColor.RED + "You didn't specify a sound effect.");
+					p.sendMessage(prefix + "Available effects are: join, quit, respawn, chat, teleport");
+				}
+				
+				if(!args[0].equalsIgnoreCase("reload") && !args[0].equalsIgnoreCase("version") && !args[0].equalsIgnoreCase("info")) {
+					p.sendMessage(incorrectArg);
+				}
+				
+			} else if(args.length > 2) {
+				p.sendMessage(incorrectArg);
+			} else if(args.length == 2 && args[0].equalsIgnoreCase("preview")) {
+				
+				if(args[1].equalsIgnoreCase("join")) {
+					p.playSound(location, events.join, 1, 0);
+				}
+				
+				if(args[1].equalsIgnoreCase("quit")) {
+					p.playSound(location, events.quit, 1, 0);
+				}
+				
+				if(args[1].equalsIgnoreCase("respawn")) {
+					p.playSound(location, events.respawn, 1, 0);
+				}
+				
+				if(args[1].equalsIgnoreCase("chat")) {
+					p.playSound(location, events.chat, 1, 0);
+				}
+				
+				if(args[1].equalsIgnoreCase("teleport")) {
+					p.playSound(location, events.tp, 1, 0);
+				}
+				
+				if(!args[1].equalsIgnoreCase("join") && !args[1].equalsIgnoreCase("quit") && !args[1].equalsIgnoreCase("respawn") && !args[1].equalsIgnoreCase("chat") && !args[1].equalsIgnoreCase("teleport")) {
+					p.sendMessage(prefix + ChatColor.RED + "Invalid sound effect.");
+					p.sendMessage(prefix + "Available effects are: join, quit, respawn, chat, teleport");
+				}
+				
+			}
+			
+			return true;
+		}
+
+		return false;
+	}
+
+}
